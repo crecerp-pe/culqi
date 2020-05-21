7199d1e87ab96bcc364232aea7fa8cce81ed8e6e
walter <wjesus88@gmail.com>
2020-05-20 20:30:29 -0500

    add acquirerpayment table payment.acquirer

--- models/models.py
+++ models/models.py
@@ -1,18 +1,9 @@
-# -*- coding: utf-8 -*-
+# -*- coding: utf-'8' "-*-"
+from odoo import models, fields, api
 
-# from odoo import models, fields, api
+class AcquirerCulqi(models.Model):
+    _inherit = 'payment.acquirer'
+    provider = fields.Selection(selection_add=[('culqi', 'Culqi')])
+    culqi_client_id = fields.Char('Culqi Client Id', size=256)
+    culqi_secret_key = fields.Char('Culqi Secret Key', size=256)
 
-
-# class payment_culqi(models.Model):
-#     _name = 'payment_culqi.payment_culqi'
-#     _description = 'payment_culqi.payment_culqi'
-
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
