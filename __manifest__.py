e34cc4ae488366900c2fa06add3d5d50b24940a5
walter <wjesus88@gmail.com>
2020-05-20 20:36:27 -0500

    add x test rama

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
         Long description of module's purpose
     """,
 
-    'author': "Crecerp",
+    'author': "Crecerp x",
     'website': "http://www.yourcompany.com",
 
     # Categories can be used to filter modules in modules listing
